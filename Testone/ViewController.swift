84099a3e0918f89326273655039a5addc2623912
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2022-01-18 15:30:04 +0300

    Initial Commit

---
+++ Testone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testone
+//
+//  Created by Afnan Theb on 15/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
